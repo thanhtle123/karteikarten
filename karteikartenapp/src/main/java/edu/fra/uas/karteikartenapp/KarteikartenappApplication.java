80f46973a0aa16446b21f52f81d2cc4ce9df9a4b
thanhtle123 <153498876+thanhtle123@users.noreply.github.com>
2024-01-05 15:24:07 +0000

    FirstKarteikartenapp

---
+++ karteikartenapp/src/main/java/edu/fra/uas/karteikartenapp/KarteikartenappApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.karteikartenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarteikartenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarteikartenappApplication.class, args);
+	}
+
+}
---
+++ karteikartenapp/src/test/java/edu/fra/uas/karteikartenapp/KarteikartenappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.karteikartenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarteikartenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
